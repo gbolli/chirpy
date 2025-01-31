836f947ba29ef61d96e02d8ff4d0fcc8602cdf98
Gord Bolli <gord@bolli.ca>
2025-01-31 11:54:43 -0800

    add metrics

--- main.go
+++ main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 func main() {
 	port := "8080"
 
 	mux := http.NewServeMux()
+	apiCfg := apiConfig{}
 
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	//mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 	mux.Handle("/assets/", http.FileServer(http.Dir("./assets")))
 	mux.HandleFunc("/healthz", healthz)
+	mux.HandleFunc("/metrics", apiCfg.metrics)
+	mux.HandleFunc("/reset", apiCfg.reset)
 
 	srv := http.Server{
 		Addr: ":" + port,
@@ -29,4 +35,27 @@ func healthz(writer http.ResponseWriter, req *http.Request) {
 	writer.Write([]byte("OK"))
 }
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, req *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(writer, req)
+	})
+}
+
+func (cfg *apiConfig) metrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %v", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	// w.Write([]byte("OK"))
+}
 // To start the server:   go build -o out && ./out
