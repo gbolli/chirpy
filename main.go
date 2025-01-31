50c7776cec337bfafc137e407245a619712d2faa
Gord Bolli <gord@bolli.ca>
2025-01-31 11:02:08 -0800

    add asset route and healthz handler

--- main.go
+++ main.go
@@ -1,18 +1,32 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
+	port := "8080"
+
 	mux := http.NewServeMux()
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	mux.Handle("/assets/", http.FileServer(http.Dir("./assets")))
+	mux.HandleFunc("/healthz", healthz)
 
 	srv := http.Server{
-		Addr: ":8080",
+		Addr: ":" + port,
 		Handler: mux,
 	}
 
-	srv.ListenAndServe()
+	log.Printf("Serving files on port %s\n", port)
+	log.Fatal(srv.ListenAndServe())
+}
+
+func healthz(writer http.ResponseWriter, req *http.Request) {
+	writer.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte("OK"))
 }
 
 // To start the server:   go build -o out && ./out
